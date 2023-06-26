888fb4839c75bba192d2f05ca9baaf19f0cf5b1e
IsaacRJimenezR <rubinjr.ti20@utsjr.edu.mx>
2023-06-25 22:39:05 -0600

    Counter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:wear/wear.dart';
+
+// Eventos
+abstract class CounterEvent {}
+
+class IncrementEvent extends CounterEvent {}
+
+class DecrementEvent extends CounterEvent {}
+
+// Cubit
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0); //STADO INICIAL EN 0
+
+//PARA CONTADOR INCREMENTAR
+  void increment() {
+    if (state < 15) {
+      emit(state + 1);
+    } else {
+      Fluttertoast.showToast(msg: '¡Límite máximo alcanzado!');
+    }
+  }
+
+//PARA CONTADOR DECREMENTAR
+  void decrement() {
+    if (state > 0) {
+      emit(state - 1);
+    } else {
+      Fluttertoast.showToast(msg: '¡Límite mínimo alcanzado!');
+    }
+  }
+
+  //PARA REINICIAR EL CONTADOR
+  void reset() {
+    emit(0);
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +48,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AmbientMode(
-      child: const MyHomePage(title: 'Flutter Demo'),
+      child: BlocProvider(
+        create: (_) =>
+            CounterCubit(), //  ´ROPORCIONA UNA INSTANCIA A TODAS LAS APPS
+        child: const MyHomePage(title: 'Flutter Demo'),
+      ),
       builder: (context, mode, child) {
         return MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
-            // PARA LOS COLORES
             visualDensity: VisualDensity.compact,
             colorScheme: mode == WearMode.active
                 ? const ColorScheme.dark(
@@ -36,50 +75,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  //VARIABLE
-  int _counter = 0;
-
-//PARA CONTADOR INCREMENTAR
-  void _incrementCounter() {
-    if (_counter < 15) {
-      setState(() {
-        _counter++;
-      });
-    } else {
-      Fluttertoast.showToast(msg: '¡Límite máximo alcanzado!'); // MENSAJE
-    }
-  }
-
-//PARA CONTADOR DECREMENTAR
-  void _decrementCounter() {
-    if (_counter > 0) {
-      setState(() {
-        _counter--;
-      });
-    } else {
-      Fluttertoast.showToast(msg: '¡Límite mínimo alcanzado!'); //MENSAJE
-    }
-  }
-
-  // para reiniciar el contador
-  void _resetCounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -87,45 +91,50 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           const SizedBox(height: 10),
           const Text(
-            'Contador:', // TEXTO DEL CONTADOR
+            'Contador:',
           ),
           const SizedBox(height: 1),
           Expanded(
             child: Center(
-              child: Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
+              // SE AGREGA EL CONTADOR
+              child: BlocBuilder<CounterCubit, int>(
+                builder: (context, state) {
+                  return Text(
+                    '$state',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                },
               ),
             ),
           ),
           SizedBox(
-            //PARA EL BOTON DEL CONTADOR
             width: 30.0,
             height: 30.0,
             child: FloatingActionButton(
-              onPressed: _decrementCounter, //VARIABLE
-              tooltip: 'Decrement', //ICONO
+              // se agrega la funcion de CUBIT CON EL DECREMENTO
+              onPressed: () => counterCubit.decrement(),
+              tooltip: 'Decrement',
               child: const Icon(Icons.remove),
             ),
           ),
           const SizedBox(height: 7),
-          //PARA REINICIAR
           SizedBox(
             width: 30.0,
             height: 30.0,
             child: FloatingActionButton(
-              onPressed: _resetCounter,
+              // se agrega el cubit con el reinicio del contador
+              onPressed: () => counterCubit.reset(),
               tooltip: 'Reset',
               child: const Icon(Icons.refresh),
             ),
           ),
           const SizedBox(height: 7),
-          //PARA DECREMENTAR
           SizedBox(
             width: 30.0,
             height: 30.0,
             child: FloatingActionButton(
-              onPressed: _incrementCounter,
+              // SE AGREGA EL CONTADOR DE INCREMENTO CON EL EVENTO DE CUBIT
+              onPressed: () => counterCubit.increment(),
               tooltip: 'Increment',
               child: const Icon(Icons.add),
             ),
